6f89af965adbcc76d831478949cdcc88c73c2201
Ioannis Papikas <papikas.ioan@gmail.com>
2017-07-04 00:56:17 +0300

    Create normal Roller class that implements basic functionality

---
+++ Roller.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the Dice Package.
+ *
+ * (c) Ioannis Papikas <papikas.ioan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Dice;
+
+/**
+ * Class AbstractRoller
+ * @package Dice
+ */
+class Roller extends AbstractRoller
+{
+    /**
+     * @var array
+     */
+    private $probabilities = [];
+
+    /**
+     * @return array
+     */
+    public function getProbabilities(): array
+    {
+        return $this->probabilities;
+    }
+
+    /**
+     * @param array $probabilities
+     */
+    public function setProbabilities(array $probabilities)
+    {
+        $this->probabilities = $probabilities;
+    }
+}
