d747664cdff48c5c5cf2e3cf093036898bc3c159
Ioannis Papikas <papikas.ioan@gmail.com>
2017-07-04 01:50:25 +0300

    Change MtRand to MTRand

---
+++ Random/MTRand.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the Dice Package.
+ *
+ * (c) Ioannis Papikas <papikas.ioan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Dice\Random;
+
+/**
+ * Class MTRand
+ * @package Dice\Random
+ */
+class MTRand extends AbstractRandomizer
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function max()
+    {
+        return mt_getrandmax();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function min()
+    {
+        return 0;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function next($min, $max, $seed = null)
+    {
+        // Check range
+        if ($min < $this->min() || $max > $this->max()) {
+            throw new \InvalidArgumentException(__METHOD__ . ': Given arguments are out of range.');
+        }
+
+        // Seed
+        mt_srand($seed);
+
+        // Return a random number
+        return mt_rand($min, $max);
+    }
+}
