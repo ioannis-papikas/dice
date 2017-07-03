95f44b40d2fcfdca5fa36b3c0e49d0ed129fb64a
Ioannis Papikas <papikas.ioan@gmail.com>
2017-07-04 00:31:50 +0300

    Add NumberHelper to support probabilities and all cases
    NumberHelper will be able to help increase accuracy using proper
    number of decimals while calculating probabilities.

---
+++ Helpers/NumberHelper.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the Dice Package.
+ *
+ * (c) Ioannis Papikas <papikas.ioan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Dice\Helpers;
+
+use Exception;
+
+/**
+ * Class NumberHelper
+ * @package Dice\Helpers
+ */
+class NumberHelper
+{
+    /**
+     * Calculate the floor of a number including decimals.
+     * It has the same functionality as floor() but it can also include decimals.
+     *
+     * @param float $number    The float number to round.
+     * @param int   $precision The number of decimals to include.
+     *
+     * @return float
+     */
+    public static function floor($number, $precision = 0)
+    {
+        $decimals = pow(10, $precision);
+
+        return floor($number * $decimals) / $decimals;
+    }
+
+    /**
+     * @param $value
+     *
+     * @return int
+     * @throws Exception
+     */
+    public static function numberOfDecimals($value)
+    {
+        // Check if value is not numeric
+        if (!is_numeric($value)) {
+            throw new Exception(__METHOD__ . ': ' . $value . ' is not a number!');
+        }
+
+        if ((int)$value == $value) {
+            return 0;
+        } else
+
+            return strlen($value) - strrpos($value, '.') - 1;
+    }
+}
---
+++ Tests/Helpers/NumberHelperTest.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the Dice Package.
+ *
+ * (c) Ioannis Papikas <papikas.ioan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Dice\Helpers\Tests;
+
+use Dice\Helpers\NumberHelper;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Class NumberHelperTest
+ * @package Dice\Helpers\Tests
+ */
+class NumberHelperTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers \Dice\Helpers\NumberHelper::floor
+     */
+    public function testFloor()
+    {
+        // No decimals
+        $this->assertEquals(10, NumberHelper::floor(10.235, 0));
+        $this->assertEquals(10, NumberHelper::floor(10.543, 0));
+        $this->assertEquals(10, NumberHelper::floor(10.987, 0));
+
+        // With decimals
+        $this->assertEquals(10.12, NumberHelper::floor(10.123456789, 2));
+        $this->assertEquals(10.12345, NumberHelper::floor(10.123456789, 5));
+        $this->assertEquals(10.12345678, NumberHelper::floor(10.123456789, 8));
+
+        $this->assertEquals(10.98, NumberHelper::floor(10.987654321, 2));
+        $this->assertEquals(10.98765, NumberHelper::floor(10.987654321, 5));
+        $this->assertEquals(10.98765432, NumberHelper::floor(10.987654321, 8));
+    }
+
+    /**
+     * @covers \Dice\Helpers\NumberHelper::numberOfDecimals
+     * @throws \Exception
+     */
+    public function testNumberOfDecimals()
+    {
+        $this->assertEquals(0, NumberHelper::numberOfDecimals(10));
+        $this->assertEquals(0, NumberHelper::numberOfDecimals(10.0));
+
+        $this->assertEquals(1, NumberHelper::numberOfDecimals(10.1));
+        $this->assertEquals(2, NumberHelper::numberOfDecimals(10.12));
+        $this->assertEquals(2, NumberHelper::numberOfDecimals(10.120));
+
+        $this->assertEquals(3, NumberHelper::numberOfDecimals(10.121));
+        $this->assertEquals(3, NumberHelper::numberOfDecimals(10.12100));
+    }
+}
