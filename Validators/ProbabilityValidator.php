8b2bb84bc17b78fd10c99eaa8d9c0ebc8f373d05
Ioannis Papikas <papikas.ioan@gmail.com>
2017-07-04 23:48:16 +0300

    Add ProbabilityValidator
    This validator makes sure that probabilities respect the fact that
    they all should sum up to 1 (or very close to 1).

---
+++ Validators/ProbabilityValidator.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the Dice Package.
+ *
+ * (c) Ioannis Papikas <papikas.ioan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Dice\Validators;
+
+use Dice\Helpers\NumberHelper;
+
+/**
+ * Class ProbabilityValidator
+ * @package Dice\Validators
+ */
+class ProbabilityValidator extends AbstractValidator
+{
+    /**
+     * @return bool
+     */
+    public function validate()
+    {
+        // Get sum and diff
+        $sum = array_sum($this->getItems());
+        $diff = abs($sum - 1);
+
+        // Check if sum is exactly 1
+        $equal = (double)$sum === 1.0 || $sum === 1;
+
+        /**
+         * Check if diff is less than a limit
+         * This functionality helps probabilities that do not
+         * sum up exactly at 1 but there is a margin of error.
+         */
+        $closeToEqual = NumberHelper::floor($diff, 10) == 0;
+
+        return $equal || $closeToEqual;
+    }
+}
